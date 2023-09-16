9022fbc79f06508300a19589e3d828188a541250
famkhan006 <famkhan006@gmail.com>
2023-09-16 11:47:58 -0700

    added several functions

---
+++ basic_functions.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * _strlen - calculate the length of a string.
+ *
+ * @s: the string to calculate it's length.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+
+/**
+ * *_strcpy - copies the string pointed to by src, including \0.
+ *@src: pointer.
+ *@dest: pointer.
+ *Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ printer.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * _puts - Prints a string
+ * @str: Pointer to the string to print
+ *
+ * Return: void
+ */
+void prints(char *str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
