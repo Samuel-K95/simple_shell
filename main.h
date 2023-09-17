3ef171d25d751625daecb2e106cc688a0bfaa239
famkhan006 <famkhan006@gmail.com>
2023-09-17 05:03:13 -0700

    updated main.h

--- main.h
+++ main.h
@@ -5,11 +5,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 
-int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
+/* basic functions  */
 int _putchar(char c);
 void prints(char *str);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+char *_strstr(char *str1, char *str2);
+char *_strdup(char *str);
+
+/*essential functions */
+
+char **split_str(char *line);
+void prompt(void);
+char *read_input(void);
+int execute_cmd(char **args, char **argv);
+
+
+/*signal */
+void handle_signal(int signal);
 
 
 #endif /* SHELL_H */
