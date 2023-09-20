0e7214ba06e6f041e01fa4fa538540e8cbaf7e1d
Samuel Keno <sgere195@gmail.com>
2023-09-20 05:46:32 -0700

    debugging

--- shelll.c
+++ shelll.c
@@ -12,6 +12,7 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 {
         char *line;
         char **args;
+	struct stat check;
         int kotari = 0, yemejemeryaw_arg_info = 0;
         (void)av;
         signal(SIGINT, handle_signal);
@@ -25,7 +26,7 @@ int main(int ac __attribute__((unused)), char **av, char **env)
                 if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
                 {
                         kotari++;
-                        yemejemeryaw_arg_info = stat(args, env);
+                        yemejemeryaw_arg_info = stat(args[0], &check);
                         execute_cmd(av, args, env, yemejemeryaw_arg_info);/* removed kotar */
                 }
                 else
