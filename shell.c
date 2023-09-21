4646e72c128a4488744f52b62be8fc073dbaff03
famkhan006 <famkhan006@gmail.com>
2023-09-20 21:26:00 -0700

    road to handle path

---
+++ shell.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * main - Simple Shell main function. Creates a prompt that reads input, parses it, executes commands,
+ * and waits for another command unless told to exit.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of arguments passed to the program.
+ * @env: The environment variables.
+ * Return: 0 upon successful execution.
+ */
+int main(int ac __attribute__((unused)), char **av, char **env)
+{
+    char *line;
+    char **args, **path;
+    int kotari = 0, huneta = 0;
+
+    (void) av;
+    signal(SIGINT, handle_signal);
+
+    for (;;)
+    {
+        prompt();
+        line = read_input(); /* Read input and return string */
+        args = split_str(line, env); /* Separate string to get command and arguments */
+
+        if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
+        {
+            kotari += 1;
+            path = search(env); /* Search for PATH in the environment variable */
+            huneta = _stat(args, path);
+            execute_cmd(av, args, env, huneta, kotari);
+        }
+        else
+        {
+            free(args);
+        }
+
+        free(line);
+    }
+
+    return (0);
+}
