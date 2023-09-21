5ffa0bf59c775c67982dc7b4d4edba67213dfa70
famkhan006 <famkhan006@gmail.com>
2023-09-21 10:53:48 -0700

    conflict

---
+++ prompt.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * prompt - prints prompt
+ * Return: void
+ */
+void prompt(void)
+{
+	char *buffer = getcwd(NULL, 0);
+
+	if (isatty(STDIN_FILENO))
+		prints("$ ");
+	free(buffer);
+}
